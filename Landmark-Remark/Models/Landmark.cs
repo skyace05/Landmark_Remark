e3001ad1b4db92dc76d73c18ada40ce87044fa94
skyace05 <32691925+skyace05@users.noreply.github.com>
2017-10-14 15:38:06 +0800

    Setup
    Setup

---
+++ Landmark-Remark/Models/Landmark.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Landmark_Remark.Models
+{
+    public class Landmark
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public string User { get; set; }
+        public string Note { get; set; }
+    }
+}
